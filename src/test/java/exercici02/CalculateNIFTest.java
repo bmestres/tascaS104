c2d7c7da1bbc3ffd62706421b78048c8f204f5da
Bernat Mestres <ber.mestres@gmail.com>
2026-06-24 11:33:33 +0200

    feat: implemented paramatrized test to ensure exceptions are thrown in any negative value input

--- src/test/java/exercici02/CalculateNIFTest.java
+++ src/test/java/exercici02/CalculateNIFTest.java
@@ -1,18 +1,17 @@
 package exercici02;
 
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.Parameter;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.params.provider.ValueSource;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class CalculateNIFTest {
 
     @ParameterizedTest
     @CsvSource({"41259309, T", "73981088, R", "19574290, W", "82036403, A", "54612791, G",
             "28495096, M", "67308494, Y", "31824693, F", "95147604, P", "46273893, D"})
-    void NumberLetterCorrespondence(int number, char letter){
+    void numberLetterCorrespondence(int number, char letter){
 
         NumLettersMap correspondenceTable = new NumLettersMap();
         CalculateNIF calculator = new CalculateNIF();
@@ -21,4 +20,15 @@ public class CalculateNIFTest {
         assertEquals(testLetter, letter);
 
     }
+
+    @ParameterizedTest
+    @ValueSource(ints = {-6, -022222, -67308494})
+    void negativeInputThrowsException(int number){
+
+        NumLettersMap correspondenceTable = new NumLettersMap();
+        CalculateNIF calculator = new CalculateNIF();
+
+        assertThrows(IllegalArgumentException.class, () -> {char testLetter = calculator.calculateLetter(number, correspondenceTable.getMap());});
+    }
+
 }
